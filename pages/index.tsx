26f1c57ee826af9fdb68d52f1f88aee8e109189f
hanocha <shumpei.s@hanolab.net>
2021-09-27 23:22:14 +0900

    Polish

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,20 @@
-import Layout from '../components/layout';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.scss'
 import Head from 'next/head';
-import Link from 'next/link';
 
-export default function Home() {
-  return (
-    // <div className="container">
-    <Layout>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Link href="/posts/first-post">
-        test
-      </Link>
-    </Layout>
-    // </div>
-  )
-}
+const Home = () => (
+  <Layout home>
+    <Head>
+      <title>{siteTitle}</title>
+    </Head>
+    <section className={utilStyles.headingMd}>
+      <p>meow.</p>
+      <p>
+        (This is a sample website - you’ll be building a site like this on{' '}
+        <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+      </p>
+    </section>
+  </Layout>
+);
+
+export default Home;
--- pages/posts/first-post.tsx
+++ pages/posts/first-post.tsx
@@ -1,21 +1,9 @@
-import Link from 'next/link';
-import Image from 'next/image';
 import Layout from '../../components/layout'
 
-export default () => {
-  return (
-    <Layout>
-      <p>
-        <h1>First post!</h1>
-      </p>
-      <p>
-        <Image src="/images/profile.jpg" width={128} height={128} />
-      </p>
-      <p>
-        <Link href="/">
-          Back
-        </Link>
-      </p>
-    </Layout>
-  )
-};
+const FirstPost = () => (
+  <Layout home={false}>
+    <h1>First post!</h1>
+  </Layout>
+)
+
+export default FirstPost;
