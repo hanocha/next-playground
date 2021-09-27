0633523bfab14c5e306327a725b5c1c3f282b1c6
hanocha <shumpei.s@hanolab.net>
2021-09-27 22:55:07 +0900

    Modulize style for layout container

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,16 +1,11 @@
+import styles from './layout.module.scss';
+
 const DefaultLayout = ({children}) => {
   return (
     <>
-      <div className="container">
+      <div className={styles.container}>
         {children}
       </div>
-      <style jsx>{`
-        .container {
-          max-width: 36rem;
-          padding: 0 1rem;
-          margin: 3rem auto 6rem;
-        }
-      `}</style>
     </>
   )
 };
